201c101aadabdf7364e49faeb4a51cd2eb9a5f23
향훈 <hyanghoon@hyanghoon-ui-MacBookPro.local>
2023-03-04 19:09:00 +0900

    chore: develop 브랜치 추가

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 5001;
 
 app.get('/', (req, res) => res.send('Hello World'));
-
 app.get('/login', (req, res) => res.send('Login'));
 
 app.listen(port, () => console.log(`on port ${port}`));
